b76996803523c6a8dbbff8630f216a96506c877b
WetzelVictor <vivou@DESKTOP-KHBBTFK.localdomain>
2017-12-27 11:36:00 -0800

    Python replicate datastructure

---
+++ victor/replicate_datastructure.py
@@ -0,0 +1,11 @@
+import os
+
+inputpath = '../data/'
+outputpath = 'output/extracted/'
+
+for dirpath, dirnames, filenames in os.walk(inputpath):
+        structure = os.path.join(outputpath, dirpath[len(inputpath):])
+        if not os.path.isdir(structure):
+            os.mkdir(structure)
+        else:
+            print("Folder does already exits!")
