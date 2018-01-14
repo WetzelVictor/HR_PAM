d23848ddb21ae7625f8e6bc17985a11e77d3a499
WetzelVictor <wetzel.victor@gmail.com>
2018-01-14 16:19:21 +0100

    DataMining implemented. It is possible to build a dataset with various spectral features extracted from librosa and YAAFE. Then it is stored in a dataStructure

---
+++ dataMining.py
@@ -0,0 +1,58 @@
+#-*-encoding:UTF8-*-
+from dataset_builder import utils
+from dataset_builder import audio_engine
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%% Computing dataStructure
+datasetFolder = 'dataset'
+dataStruct = utils.build_data_structure(datasetFolder)
+dataStruct = audio_engine.compute_features(dataStruct)
+Nx = len(dataStruct["filepath"])
+
+#%% Plots
+Labels = ['fil','plectre']
+#Labels = ['nylon','acier']
+plotX = 'SpectralRolloffMean'
+plotY = 'SpectralBandwidthMean'
+
+# A FAIRE
+classesIncludeOnly = ['noise','fil']
+# A FAIRE
+
+# log ou None
+plotType = None
+
+# INIT
+XX = {}
+YY = {}
+
+# LOOP
+for tag in Labels:
+    XX[tag] = []
+    YY[tag] = []
+
+for i in xrange(Nx):
+    curLabel = dataStruct['assigned_label'][i]
+    
+    if not "sinus" in curLabel:
+        continue
+    
+    for tag in Labels:
+        if tag in curLabel:
+            XX[tag].append(dataStruct[plotX][i])
+            YY[tag].append(dataStruct[plotY][i])
+
+# PLOT PLOT PLOT PLOT PLOT PLOT PLOT PLOT PLOT PLOT PLOT PLOT PLOT PLOT PLO
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.figure(figsize=(12,7))  
+for tag in Labels:
+    if plotType == 'log':
+        plt.scatter(10*np.log(XX[tag]), 10*np.log(YY[tag]))
+    elif plotType is None:
+        plt.scatter(XX[tag],YY[tag])
+        
+plt.xlabel(plotX,fontsize=20)
+plt.ylabel(plotY,fontsize=20)
+plt.legend(Labels,fontsize=18)
